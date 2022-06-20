07bf2e2aa4e7390ec33ba2918194ed1cbaf16227
kyoohyun <kgh2120@naver.com>
2022-06-20 11:02:32 +0900

    jpa 기능 가능하게 수정

--- src/main/java/com/example/restfulservice/user/User.java
+++ src/main/java/com/example/restfulservice/user/User.java
@@ -6,14 +6,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
-@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // Swagger에서 설명을 위한 annotation
+@Entity
 public class User {
+    @Id @GeneratedValue
     private Integer id;
 
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1, sysdate(), 'User1','test111', '123412-1234123');
+insert into user values(2, sysdate(), 'User2','test222', '123412-1234123');
+insert into user values(3, sysdate(), 'User3','test3', '123412-1234123');
+insert into user values(4, sysdate(), 'User4','test4', '123412-1234123');
