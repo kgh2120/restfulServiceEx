aae384f12d2d2fac8bf12fac4aeb69df7cdb09ba
kyoohyun <kgh2120@naver.com>
2022-06-20 10:29:33 +0900

    잡다한 기능 추가

--- src/main/java/com/example/restfulservice/user/User.java
+++ src/main/java/com/example/restfulservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulservice.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +12,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
+
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
+    
     private LocalDateTime joinDate;
 
+    @ApiModelProperty(notes = "사용자 비밀번호을 입력해 주세요.")
     private String password;
+
+    @ApiModelProperty(notes = "사용자 주민등록번호을 입력해 주세요.")
     private String ssn;
 
 }
